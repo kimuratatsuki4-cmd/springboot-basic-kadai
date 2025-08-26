f17a3c8025cce11386838297473f2ce262ed59b3
kimuratatsuki4-cmd <kimuratatsuki4@gmail.com>
2025-08-26 10:54:40 +0900

    一部修正

--- spring_kadai_form/src/main/java/com/example/springkadaiform/controller/ContactFormController.java
+++ spring_kadai_form/src/main/java/com/example/springkadaiform/controller/ContactFormController.java
@@ -1,6 +1,5 @@
 package com.example.springkadaiform.controller;
 
-import org.springframework.core.Conventions;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
@@ -14,23 +13,24 @@ import com.example.springkadaiform.form.ContactForm;
 public class ContactFormController {
 
 	@GetMapping("/form")
-	public String login() {
-		return "/contactFormView";
+	public String showForm() {
+		return "contactFormView";
 	}
 
-	@PostMapping("/form")
+	@PostMapping("/confirm")
 	public String registerUser(RedirectAttributes redirectAttributes,
 			@Validated ContactForm form, BindingResult result) {
 		// バリデーションNGの場合は/formへリダイレクト
 		if (result.hasErrors()) {
 			redirectAttributes.addFlashAttribute("ContactForm", form);
-			redirectAttributes.addFlashAttribute(BindingResult.MODEL_KEY_PREFIX
-					+ Conventions.getVariableName(form), result);
-			return "redirect:/contactformView";
+			//	バリデーションエラーが発生した際に、リダイレクト先の画面でもエラーメッセージを表示したい場合に実装		
+//			redirectAttributes.addFlashAttribute(BindingResult.MODEL_KEY_PREFIX
+//					+ Conventions.getVariableName(form), result);
+			return "contactFormView";
 		} else {
-			//	バリデーションOKの場合は/confirmのまま、確認画面に入力データが表示される
+			//	バリデーションOKの場合は/confirmのまま、確認画面（リダイレクト先）に入力データが表示される
 			redirectAttributes.addFlashAttribute("ContactForm", form);
-			return "redirect:/confimView";
+			return "redirect:/confirmView";
 		}
 
 	}
